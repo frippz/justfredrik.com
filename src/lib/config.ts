789db7e75a44708d13d47229f15a707f949091e8
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-15 09:43:10 +0100

    chore: remove old project files

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment';
-
-export const title = 'justfredrik.com';
-export const description = 'My own little SvelteKit blog';
-export const url = dev ? 'http://localhost:5173/' : 'https://justfredrik.com/';
